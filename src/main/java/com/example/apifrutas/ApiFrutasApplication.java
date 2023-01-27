1ef092a47e3a1b81e8fff9b0493474a924dc56db
Matheus Camargo <sti@matera.com>
2023-01-27 18:11:27 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/apifrutas/ApiFrutasApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifrutas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFrutasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFrutasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apifrutas/ApiFrutasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apifrutas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFrutasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
